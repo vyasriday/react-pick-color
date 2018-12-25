80be2bffbe1e8fca2098138e7a1648a297fa503e
Hridayesh Sharma <vyasriday7@gmail.com>
2018-12-25 21:56:42 +0530

    Updates

---
+++ precache-manifest.6a9bbfbebabb27a520ce9b67f01dd9a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3ab65142927ddb6516d",
+    "url": "/react-pick-color/static/js/runtime~main.e3ab6514.js"
+  },
+  {
+    "revision": "06a0195511ac5e071af2",
+    "url": "/react-pick-color/static/js/main.06a01955.chunk.js"
+  },
+  {
+    "revision": "9d65a0d5e5a889113337",
+    "url": "/react-pick-color/static/js/1.9d65a0d5.chunk.js"
+  },
+  {
+    "revision": "06a0195511ac5e071af2",
+    "url": "/react-pick-color/static/css/main.a3e00c3c.chunk.css"
+  },
+  {
+    "revision": "137fc6dd44addadabc1782c04865d587",
+    "url": "/react-pick-color/index.html"
+  }
+];
